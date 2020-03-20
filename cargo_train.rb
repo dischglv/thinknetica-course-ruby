7b5a0e7dd31e66c1809e58eb32b1b7bbd5f0ff7b
Diana Schegoleva <dschegoleva@gmail.com>
2020-03-20 23:45:33 +0300

    added require to train classes

--- cargo_train.rb
+++ cargo_train.rb
@@ -1,6 +1,7 @@
+require_relative 'train.rb'
+
 class CargoTrain < Train
   def hitch_wagon(wagon)
     super(wagon) if wagon.class == 'CargoWagon'
   end
-
 end
--- passenger_train.rb
+++ passenger_train.rb
@@ -1,3 +1,5 @@
+require_relative 'train.rb'
+
 class PassengerTrain < Train
   def hitch_wagon(wagon)
     super(wagon) if wagon.class == 'PassengerWagon'
