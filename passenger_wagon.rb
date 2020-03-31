a95c19c5f59786043cf39fdb0a0c60fe69bb924c
Diana Shchegoleva <dschegoleva@gmail.com>
2020-03-31 09:56:44 +0300

    added seats to passenger wagons

--- passenger_wagon.rb
+++ passenger_wagon.rb
@@ -1,4 +1,28 @@
 require_relative 'wagon.rb'
 
 class PassengerWagon < Wagon
+    attr_reader :seats, :empty_seats
+
+    def initialize(number, seats)
+        @seats = seats
+        super(number)
+        @empty_seats = seats
+    end
+
+    def take_seat
+        self.empty_seats -= 1 unless self.empty_seats == 0
+    end
+
+    def occupied_seats
+        self.seats - self.empty_seats
+    end
+
+    protected
+    attr_writer :seats, :empty_seats
+
+    def validate!
+        super
+        raise "Количество мест должно быть не nil" if self.seats.nil?
+        raise "Количество мест должно быть неотрицательным числом" if self.seats < 0
+    end
 end
