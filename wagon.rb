fe5258c21db7dc13ec09bb764f6e3d9563887b05
Diana Shchegoleva <dschegoleva@gmail.com>
2020-03-31 09:56:01 +0300

    added number and validations to wagon

--- wagon.rb
+++ wagon.rb
@@ -1,3 +1,18 @@
 class Wagon
   include Manufactured
+  include Validation
+
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+    validate!
+  end
+
+  protected
+  attr_writer :number
+
+  def validate!
+    raise "Номер поезда не может быть nil" if self.number.nil?
+  end
 end
